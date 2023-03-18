250587439ea1db115a38f172b3fb630c79113a3f
Nadia <ivashchenko.nadia@gmail.com>
2023-03-18 15:33:33 +0200

    Initial Commit

---
+++ NotesApp_SUI/NotesApp_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp_SUIApp.swift
+//  NotesApp_SUI
+//
+//  Created by Students on 18.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
